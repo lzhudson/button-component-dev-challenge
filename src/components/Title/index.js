046b0715ba73d58112e1e7ae740a4cd262ddef01
Hudson Holanda <hudson.holanda@wedev.software>
2022-04-06 19:19:09 -0300

    feat: create component Title

---
+++ src/components/Title/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-family: ${(props) => props.theme.fonts.poppins};
+  font-size: 24px;
+  line-height: 36px;
+  color: #4F4F4F;
+  font-weight: 500;
+  margin-top: 53px;
+  margin-bottom: 28px;
+`;
